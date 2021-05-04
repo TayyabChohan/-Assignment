b890f9da59f08de310d62c0b23da9d3cebb2c91e
TayyabChohan <TayyabChohan7@gmail.com>
2021-05-04 17:37:58 +0500

    qustion 2

---
+++ question2.js
@@ -0,0 +1,25 @@
+function Equilibium(arr, n){
+    var ftsum; 
+    var rsum;
+    var i,j;
+         for(i=0; i<n; ++i)
+         {
+             ftsum = 0;
+              for(let j=0; j<i; j++)
+              ftsum += arr[j];
+               
+              rsum = 0;
+              for(let j=i+1; j<n; j++)
+              rsum +=arr[j];
+               
+             
+              if(ftsum == rsum)
+                 return arr[i];
+         }
+          
+            return -1;
+}
+var arr= [2,2,25,1,3]
+n=arr.length;
+Equilibium(arr,n)
+console.log(Equilibium(arr,n))
